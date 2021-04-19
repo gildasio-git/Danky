3f48a369ff6e547428832f3305d9aa94ac134edc
Gildasio B. Brito <gildasio.ariquemes@hotmail.com>
2021-04-19 18:28:26 -0400

    Tema 1/10

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet"> 
+    <title>Tema - DK</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section class="topo">
+        <div class="center">
+            <header>
+                <div class="logo">
+                    <img src="images/logo.png" alt="">
+                </div><!--logo -->
+            </header><!--header -->
+            <ul class="menu-desktop">
+                 <li><a href="">Home</a></li>
+                 <li><a href="">Sobre</a></li>
+                 <li><a href="">Contato</a></li>
+            </ul><!-- menu -->
+            <div class="clear"></div>
+            <br />
+            <br />
+            <div class="w50 time-descricao">
+                <h2>Melhore a comunicação com seu cliente e time</h2>
+                <p>Consultoria especializada em statups, empresas, principalmente, pessoas</p>
+                <a target="_blank" href="https://dankicode.com">Ver demonstração</a>
+            </div><!--w50 -->
+            <div class="w50 time-imagem">
+                <img src="images/equipe.png">
+            </div><!-- w50-2 -->
+            <div class="clear"></div>
+         </div> <!-- center -->
+    </section><!-- topo -->
+</body>
+</html>
+
